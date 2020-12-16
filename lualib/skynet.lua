aecaa9a7745bdbfa362d773ed8088622a707de63
2468180623@qq.com <2468180623@qq.com>
2020-12-16 06:07:23 +0000

    websocket upgrade

--- lualib/skynet.lua
+++ lualib/skynet.lua
@@ -248,7 +248,7 @@ local coroutine_pool = setmetatable({}, { __mode = "kv" })
 local function co_create(f)
 	local co = tremove(coroutine_pool)
 	if co == nil then
-		co = coroutine_create(function(...)
+		co = coroutine.create(function(...)
 			f(...)
 			while true do
 				local session = session_coroutine_id[co]
